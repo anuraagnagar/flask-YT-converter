70b7d932323c844c2ba30288c39e0afc40d98e55
Anurag Nagar <nagaranurag1999@gmail.com>
2023-07-10 20:42:36 +0530

    Adding about page rout

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(port=5500)
