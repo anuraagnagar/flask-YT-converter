d07197e5817408099eb305e5b6caa3583cecaacb
Anurag Nagar <nagaranurag1999@gmail.com>
2023-10-14 10:47:57 +0530

    Format: code and remove extra code yttmp3 module.

--- app.py
+++ app.py
@@ -1,27 +1,6 @@
-from flask import Flask
-from flask import render_template, request, redirect, url_for, flash
+from yttmp3 import create_app
 
-import os
-
-app = Flask(__name__, template_folder="templates", static_folder="static")
-
-app.config["DEBUG"] = True
-app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
-
-
-@app.route("/", methods=["GET", "POST"])
-@app.route("/home", methods=["GET", "POST"])
-def index():
-    return render_template("index.html")
-
-@app.route('/how_to_use')
-def how_to_use():
-    return render_template('how_to_use.html')
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
+app = create_app()
 
 if __name__ == "__main__":
-    app.run(port=5500)
+    app.run()
